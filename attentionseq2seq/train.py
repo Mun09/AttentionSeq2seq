ed630cd282b4a05f797f6c824100632af1a8df21
Mun Jaeyoung <77109631+Mun09@users.noreply.github.com>
2024-06-10 23:36:28 +0900

    Add files via upload

---
+++ attentionseq2seq/train.py
@@ -0,0 +1,10 @@
+import sys, os
+parent_dir = os.path.join(os.path.dirname(__file__), "..")
+sys.path.append(parent_dir)
+
+from Seq2seq import *
+from common.np import *
+import matplotlib.pyplot as plt
+
+
+
